3aa93be2f82042a05fe60462d17897bec912991d
Nematjon <frank.sport.93@gmail.com>
2019-05-19 11:02:02 +0300

    modernizr.js added

---
+++ gulp/tasks/modernizr.js
@@ -0,0 +1,12 @@
+let gulp = require("gulp"),
+  modernizr = require("gulp-modernizr");
+
+gulp.task("modernizr", function() {
+  return gulp.src(["./app/assets/styles/**/*.css", "./app/assets/scripts/**/*.js"])
+   .pipe(moderniizr({
+     "options": [
+      "setClasses"
+     ]
+   })
+   .pipe(gulp.dest("./app/temp/scripts/"));
+});
